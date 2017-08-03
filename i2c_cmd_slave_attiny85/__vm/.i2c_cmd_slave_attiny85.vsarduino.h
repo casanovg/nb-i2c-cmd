a9d3518b1b028ae2898b93c5d139b20eee592299
Gustavo Casanova <gustavo.casanova@outlook.com>
2017-08-03 01:33:07 -0300

    ADC in DC mode ...

--- i2c_cmd_slave_attiny85/__vm/.i2c_cmd_slave_attiny85.vsarduino.h
+++ i2c_cmd_slave_attiny85/__vm/.i2c_cmd_slave_attiny85.vsarduino.h
@@ -5,7 +5,7 @@
 			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Digispark (Default - 16.5mhz), Platform=avr, Package=digistump
+	Hardware: ATtiny25/45/85 w/ ATtiny85, Platform=avr, Package=attiny
 */
 
 #if defined(_VMICRO_INTELLISENSE)
@@ -14,9 +14,9 @@
 #define _VSARDUINO_H_
 #define __AVR_ATtiny85__
 #define __AVR_ATTINY85__
-#define F_CPU 16500000L
-#define ARDUINO 10801
-#define ARDUINO_AVR_DIGISPARK
+#define F_CPU 1000000L
+#define ARDUINO 10803
+#define ARDUINO_attiny
 #define ARDUINO_ARCH_AVR
 #define __cplusplus 201103L
 #define __AVR__

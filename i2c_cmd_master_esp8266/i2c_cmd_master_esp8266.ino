6c4bfbc16f704764756d959213e8ea2387dda82d
Gustavo Casanova <gustavo.casanova@outlook.com>
2018-08-06 18:19:26 -0300

    Update to v0.9

--- i2c_cmd_master_esp8266/i2c_cmd_master_esp8266.ino
+++ i2c_cmd_master_esp8266/i2c_cmd_master_esp8266.ino
@@ -5,8 +5,8 @@
 // *  ..................................................  *
 // *  Author: Gustavo Casanova                            *
 // *  ..................................................  *
-// *  Firmware Version: 0.8 | MCU: ESP8266                *
-// *  2018-07-25 gustavo.casanova@nicebots.com            *
+// *  Firmware Version: 0.9 | MCU: ESP8266                *
+// *  2018-08-07 gustavo.casanova@nicebots.com            *
 // ********************************************************
 //
 // Run this master program on a NodeMCU, ESP-01 or ESP-12 Module
